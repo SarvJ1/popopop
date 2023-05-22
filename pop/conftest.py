7d5b9b54d2b41000ed1e6a4df349b183d8af5ea9
Test <test@harness.io>
2023-05-22 11:57:26 +0000

    Initial Django Service Setup

---
+++ pop/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pop.users.models import User
+from pop.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
